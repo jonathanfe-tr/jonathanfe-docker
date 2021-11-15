38e6dcc940d6f7704588be86843b4fc3e1686cdb
Jonathan Feldman <jonathanfe@etoro.com>
2021-11-15 19:29:39 +0200

    fucking tired

---
+++ .github/actions/deploy/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+mkdir ~/.ssh
+echo $VAULT_PASS > /vault_password_file.txt
+ansible-vault decrypt \
+--vault-password-file=/vault_password_file.txt \
+id_rsa-encrypted \
+--output=~/.ssh/id_rsa
+chmod 0600 ~/.ssh/id_rsa
+ansible-playbook --vault-password-file=/vault_password_file.txt \
+-i inventory.yml \
+docker-playbook.yml
+
